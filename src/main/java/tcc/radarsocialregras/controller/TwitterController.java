dcbca64fca5710fe30329bab1675b89573a4f9c1
idea-PC\Patricia <patriciasoramos@gmail.com>
2017-07-30 12:33:05 -0300

    Adicionado repositorio e controller do twitter

--- src/main/java/tcc/radarsocialregras/controller/TwitterController.java
+++ src/main/java/tcc/radarsocialregras/controller/TwitterController.java
@@ -1,8 +1,18 @@
 package tcc.radarsocialregras.controller;
 
+import java.text.ParseException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import tcc.radarsocialregras.repository.FacebookRepository;
+import tcc.radarsocialregras.repository.TwitterRepository;
 
 @Controller
 public class TwitterController {
@@ -10,5 +20,32 @@ public class TwitterController {
 	public void index(){ 
 		System.out.println("Carregando o twitter"); 
 //		return "index";
+	}
+	
+	@RequestMapping(value = "/twitterPortais", method = RequestMethod.POST,produces = "application/json")
+	public @ResponseBody String facebookPortais(@RequestBody String body){ 
+		
+		JSONArray array = new JSONArray(body); 
+		String response = null;
+		for(int i=0; i<array.length(); i++){
+		    JSONObject jsonObj = array.getJSONObject(i);
+		    response = TwitterRepository.getPortais(jsonObj.getString("portal"),jsonObj.getString("dataInicial"),jsonObj.getString("dataFinal"),jsonObj.getString("link"));
+		}
+		return response.toString();
+	}
+	
+	@RequestMapping(value = "/twitterSearch", method = RequestMethod.POST,produces = "application/json")
+	public @ResponseBody String buscaPorFiltros(@RequestBody String body) throws JSONException, ParseException{ 
+		
+//		String[] portal = body.split(":");
+		
+		JSONArray array = new JSONArray(body); 
+		String response = null;
+		for(int i=0; i<array.length(); i++){
+		    JSONObject jsonObj = array.getJSONObject(i);
+		    response = TwitterRepository.buscaPorFiltro(jsonObj.getString("portal"),jsonObj.getString("dataInicial"),jsonObj.getString("dataFinal"),jsonObj.getString("link"));
+		}
+		
+		return response.toString();
 	}
 }
---
+++ src/main/java/tcc/radarsocialregras/repository/TwitterRepository.java
@@ -0,0 +1,45 @@
+package tcc.radarsocialregras.repository;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+
+import com.mongodb.AggregationOutput;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
+import tcc.radarsocial.dao.FacebookDao;
+import tcc.radarsocial.dao.TwitterDao;
+import tcc.radarsocial.model.Pagina;
+import tcc.radarsocial.model.PostFacebook;
+import tcc.radarsocialregras.util.Util;
+
+public class TwitterRepository {
+
+	public static String getPortais(String portal,String dataInicial,String dataFinal,String link){
+		
+		
+		TwitterDao dao = new TwitterDao();
+		AggregationOutput portais = dao.buscarTodosPortais(portal,dataInicial,dataFinal,link);
+		
+//		String serialize = JSON.serialize(portais);
+	    System.out.println(portais.toString());
+		return portais.toString();		
+		
+	}
+	
+	public static String buscaPorFiltro(String portal,String dataInicial, String dataFinal, String link) throws ParseException{
+		
+		
+		TwitterDao dao = new TwitterDao();		
+		DBCursor cursor = dao.buscaPorFiltro(portal,dataInicial,dataFinal,link);
+
+        String serialize = JSON.serialize(cursor);
+        System.out.println(serialize);
+		return serialize;
+		
+	}
+}
