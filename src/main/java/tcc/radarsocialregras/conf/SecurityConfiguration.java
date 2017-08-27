449d563ceb58d573d733f3b6f0a4cf02a11a7de1
idea-PC\Patricia <patriciasoramos@gmail.com>
2017-08-27 12:11:32 -0300

    Adicionada tela de login - loginainda não funcionando

--- src/main/java/tcc/radarsocialregras/conf/SecurityConfiguration.java
+++ src/main/java/tcc/radarsocialregras/conf/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package tcc.radarsocialregras.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,21 +14,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
-	@Override 
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**"); 
-	}
+//	@Override 
+//	public void configure(WebSecurity web) throws Exception {
+//		web.ignoring().antMatchers("/resources/**"); 
+//	}
+	
+//	@Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//    }
 
 	@Override 
 	protected void configure(HttpSecurity http) throws Exception { 
 		http.authorizeRequests() 
 		.antMatchers("/").permitAll() 
 		.anyRequest().authenticated() 
-		.and() 
-		.formLogin().and() 
-		.httpBasic(); 
+		.and().formLogin();
+
 	}
-	
+	@Autowired
 	private UserDetailsService users;
 	
 	@Override
